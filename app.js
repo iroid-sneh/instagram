d4ae727f0fa526487a446ac2ba93b0ba9fa3f41e
Your GitHub Username <Your GitHub Email>
2025-08-26 18:40:30 +0530

    feat: added user Schema

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 db.sequelize
     .sync()
     .then(() => {
---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+import { DataTypes, Model } from "sequelize";
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
